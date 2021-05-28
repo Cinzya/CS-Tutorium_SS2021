3e97ec6a157bbb0b1ddb55eac115b94b7a9b439c
Cinzya <cynti-ebert@web.de>
2021-05-28 05:23:06 +0200

    include studiengänge

--- neu.php
+++ neu.php
@@ -1,5 +1,6 @@
 <html lang="de">
 <?php include("verarbeiten.php");?>
+<?php include("studiengaenge.verarbeiten.php");?>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
