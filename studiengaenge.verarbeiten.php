dba6cfe34edc88c62fe0c3b01a9ad79c58dfb4f8
Cinzya <cynti-ebert@web.de>
2021-05-28 05:20:37 +0200

    löschen funktion

--- studiengaenge.verarbeiten.php
+++ studiengaenge.verarbeiten.php
@@ -1,7 +1,19 @@
 <?php
     $mysqli = new mysqli("localhost", "root", "", "studentenliste") or die(mysqli_error($mysqli));
+                
+    $neuerStudiengang = false;
+    $bearbeiteStudiengang = false;
 
+    $id = "";
+    $name = "";
+    $kurzform = "";
+    $regelstudienzeit = "";
+
+    if (isset($_GET["loeschen"])){
+        $id = $_GET["loeschen"];
+        $mysqli->query("DELETE FROM studiengang WHERE id=$id")
+        or die($mysqli->error);
+    }
+    
     $studiengaenge = $mysqli->query("SELECT * FROM studiengang");
-
-
 ?>
