81bb1bc764f995954fd1b1f6c225320b233fc5ff
Cinzya <cynti-ebert@web.de>
2021-06-11 05:17:11 +0200

    ADD :: login page

---
+++ login.php
@@ -0,0 +1,25 @@
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <title>Studentenliste</title>
+</head>
+<body>
+<div class="container">
+    <h1>Anmeldung</h1>
+    <form>
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">E-Mail-Addresse</label>
+        <input type="email" name="email" class="form-control" id="exampleInputEmail1">
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Passwort</label>
+        <input type="password" name="passwort" class="form-control" id="exampleInputPassword1">
+    </div>
+    <button type="submit" name="anmelden" class="btn btn-primary">Anmelden</button>
+    </form>
+</div>
+</body>
+</html>
